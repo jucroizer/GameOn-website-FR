ba1e575b3d860c9e0604f37ccf558d4133b50bab
Justine Croizer <justine.croizer@icloud.com>
2022-01-25 10:50:52 +0100

    Premieres modifications du CSS et de l'HTML

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,20 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//close modale event
+closeBtn.addEventListener("click", closeModal);
 
+function closeModal(){
+  modalbg.style.display = "none";
+}
+
+// Récupération du nom et log
+var nom = document.getElementById('last').value;
+console.log(nom);
+// Vérifier si le nom contient bien que des lettres et les espaces
+const verif_nom = /^[a-zA-Zéī\ ]+$/;
+if(verif_nom.test(nom)){
+  console.log(true);
+}else{
+  console.log(false);
+}
