d24a88b3eb7474c31fa388e1aae29c970f5edd6a
Justine Croizer <justine.croizer@icloud.com>
2022-02-22 20:27:44 +0100

    css telephone termine

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -34,16 +34,16 @@ function closeModal(){
 
 // Tableau de booleens pour la vérification
 let verifTab = [];
-console.log(verifTab);
 const btn_submit = document.getElementById('submit');
 
-
-
+// au click sur le bouton "C'est parti" on appelle la fonction submissionForm
 btn_submit.addEventListener('click', submissionForm);
 
 function submissionForm(){
 
   const modal_body = document.getElementById('modal_body');
+
+  // Appels de toutes les fonctions de vérification
   check_prenom();
   check_nom();
   check_email();
@@ -52,6 +52,7 @@ function submissionForm(){
   check_location();
   check_condition();
 
+  // vérification que tous les champs des formulaires sont true
   let countTrue = 0;
 
   for(let i = 0; i < verifTab.length; i++){
@@ -74,6 +75,7 @@ function submissionForm(){
     modal_body.style.display = "block";
     
   }else{
+    // en cas d'erreur trouvé dans le formulaire on ne ferme pas la modale et on affiche les messages d'erreurs
     event.preventDefault();
     false;
   }
@@ -85,183 +87,154 @@ function submissionForm(){
   function valideModal(){
     modalbg.style.display = "none";
   }
-} 
-// console.log(submissionForm());
+}
 
+/**
+*  Vérification du Prénom
+*/
 function check_prenom(){
-    /**
-    * Vérification du Prénom
-    */
+    
     let prenom = document.getElementById("first").value;
     let error_first = document.getElementById('error_first');
-    const verif_prenom = /^[a-zA-Zéī\ ]+$/;
+    const verif_prenom = /^[a-zA-Zéī\ ]+$/; // ne prend en compte que les lettres et les espaces
 
+    // si la regex du prénom est correct et que le prenom est supérieur ou égale à 2 caractères
     if(verif_prenom.test(prenom) && prenom.length >= 2){
+      // je n'affiche pas d'erreur et j'incrémente à true verifTab pour la vérification
       error_first.style.display = "none";
-      console.log(true);
+      
       verifTab[0] = true;
     }else{
+      // sinon j'affiche l'erreur et j'incrémente à false verifTab pour la vérification
       error_first.style.display = "block";
-      console.log(false);
+      document.getElementById('first').style.border = "solid 1px #FE142F";
       verifTab[0] = false;
     }
-    console.log('Le prénom est il true ou false ?' + verifTab[0]);
 }
   
+/**
+  * Vérification du Nom
+  */
 function check_nom(){
-  /**
-   * Vérification du Nom
-   */
-  // Récupération du nom et log
+  
   let nom = document.getElementById('last').value;
   let error_last = document.getElementById('error_last');
   const verif_nom = /^[a-zA-Zéī\ ]+$/; // Vérifier si le nom contient bien que des lettres et les espaces
 
+   // si la regex du nom est correct et que le nom est supérieur ou égale à 2 caractères
   if(verif_nom.test(nom)  && nom.length >= 2){
+    // je n'affiche pas d'erreur et j'incrémente à true verifTab pour la vérification
     error_last.style.display = "none";
-    console.log(true);
     verifTab[1] = true;
   }else{
+    // sinon j'affiche l'erreur et j'incrémente à false verifTab pour la vérification
     error_last.style.display = "block";
-    console.log(false);
+    document.getElementById('last').style.border = "solid 1px #FE142F";
     verifTab[1] = false;
   }
-  console.log('Le nom est il true ou false ?' + verifTab[1]);
 }
 
+/**
+  * Vérification de l'email
+  */
 function check_email(){
-  /**
-   * Vérification de l'email
-   */
-  // Recuperation de l'email et log
+  
   let email = document.getElementById('email').value;
   let error_email = document.getElementById('error_email');
 
   const verif_email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g; // \w comprend toutes les lettres et underscores et comprend les - et les . Comprend en suivant le @ 
   
+  // si la regex du mail est correct
   if(verif_email.test(email)){
+    // je n'affiche pas d'erreur et j'incrémente à true verifTab pour la vérification
     error_email.style.display = "none";
-    console.log(true);
     verifTab[2] = true;
   }else{
+    // sinon j'affiche l'erreur et j'incrémente à false verifTab pour la vérification
     error_email.style.display = "block";
-    console.log(false);
+    document.getElementById('email').style.border = "solid 1px #FE142F";
     verifTab[2] = false;
-    // console.error('Veuillez entrer une adresse email valide')
   }
-  console.log('L\'email est il true ou false ?' + verifTab[2]);
 }
 
+/**
+  * Vérification de la date de naissance
+  */
 function check_birthdate(){
-  /**
-   * Vérification de la date de naissance
-   */
-
+  
    let birthdate = document.getElementById('birthdate').value;
    let error_birthdate = document.getElementById('error_birthdate');
-  //  console.log(birthdate);
-  //  console.log(birthdate.length);
  
+   // récupération de la date entrée par l'utilisateur
    const day = new Date(birthdate).getDate();
    const month = new Date(birthdate).getMonth()+1;
    const year = new Date(birthdate).getFullYear();
    let inputBirthdate = new Date (year, month, day).getTime();
  
+   // récupération de la date du jour
    let thisMonth = new Date().getMonth()+1;
    let thisDay = new Date().getDate();
    let thisYear = new Date().getFullYear();
    let now = new Date (thisYear, thisMonth, thisDay).getTime();
 
+   // si la date actuelle moins la date entrée est supérieur ou égale à 0 
    if((now - inputBirthdate) >= 0){
+     // je n'affiche pas d'erreur et j'incrémente à true verifTab pour la vérification
     error_birthdate.style.display = "none";
-    console.log((now - inputBirthdate) >= 0);
     verifTab[3] = true;
    }else{
+     // sinon j'affiche l'erreur et j'incrémente à false verifTab pour la vérification
      error_birthdate.style.display = "block";
-     console.log(false);
+     document.getElementById('birthdate').style.border = "solid 1px #FE142F";
      verifTab[3] = false;
    }
-
-   console.log('La date de naissance est elle true ou false ?' + verifTab[3]);
-  //  console.log(thisDay + '/' + thisMonth + '/' + thisYear);
-  //  console.log(day + '/' + month + '/' + year); 
-  //  if(!birthdate.length > 0){
-  //   error_birthdate.style.display = "block";
-  //   console.log(false);
-  //  }else{
-  //     // is ok - pour ne pas aller dans le futur
-  //     if(day > thisDay && month > thisMonth && year < thisYear){
-  //       error_birthdate.style.display = "none";
-  //       console.log(true);
-  //     } else if(day > thisDay && month >= thisMonth && year >= thisYear){
-  //       error_birthdate.style.display = "block";
-  //       console.log(false);
-  //     }
-    
-  //     // is ok - pour ne pas aller dans le futur
-  //     if(month > thisMonth && year < thisYear){
-  //       error_birthdate.style.display = "none";
-  //       console.log(true);
-  //     } else if (month > thisMonth && year >= thisYear) {
-  //       error_birthdate.style.display = "block";
-  //       console.log(false);
-  //     }
-    
-  //     //is ok - pour ne pas aller dans le futur
-  //     if(year > thisYear){
-  //       error_birthdate.style.display = "block";
-  //       console.log(false);
-  //     }
-  //  }
- 
 }
 
+/**
+  * Vérification du nombre de tournoi joués
+  */
 function check_quantity(){
-  /**
-   * Vérification du nombre de tournoi joués
-   */
+  
    let quantity = document.getElementById("quantity").value;
    let error_quantity = document.getElementById('error_quantity');
-   console.log(quantity.length);
-   console.log(typeof quantity);
-   console.log(quantity);
 
+   // si le nombre de tournoi est conpris entre 0 et 99 et qu'il n'est pas null
    if(quantity >= 0 && quantity <= 99 && quantity != ''){
+    // je n'affiche pas d'erreur et j'incrémente à true verifTab pour la vérification
      error_quantity.style.display = "none";
-     console.log(true);
      verifTab[4] = true;
    }else{
+     // sinon j'affiche l'erreur et j'incrémente à false verifTab pour la vérification
     error_quantity.style.display = "block";
-    console.log(false);
+    document.getElementById('quantity').style.border = "solid 1px #FE142F";
     verifTab[4] = false;
    }
-   console.log('Le nombre de tournoi est il true ou false ?' + verifTab[4]);
 }
 
+ /**
+  * Vérification checkbox - localisation
+  */
 function check_location(){
-  /**
-   * Vérification checkbox - localisation
-   */
-   let radio_input = document.getElementsByName('location');
-   let error_location = document.getElementById('error_location');
-  //  console.log(radio_input);
  
-   for (var i = 0; i < radio_input.length; i++) {
-     // do whatever you want with the checked radio
+  let radio_input = document.getElementsByName('location');
+  let error_location = document.getElementById('error_location');
+  
+  // pour chaque radio bouton je vérifie 
+   for (let i = 0; i < radio_input.length; i++) {
+
+    // si un radio bouton est selectionné
      if(radio_input[i].checked){
+       // je n'affiche pas d'erreur et j'incrémente à true verifTab pour la vérification
        error_location.style.display = "none";
-       console.log(true);
-      //  console.log(radio_input[i].value);
        verifTab[5] = true;
+       // je stop la boucle ici
        break;
      }else{
+       // sinon j'affiche l'erreur et j'incrémente à false verifTab pour la vérification
        error_location.style.display = "block";
-      //  console.log(radio_input[i].value);
-       console.log(false);
        verifTab[5] = false;
      }
    }
-   console.log('La localisation du tournoi est elle true ou false ?' + verifTab[5]);
 }
 
 function check_condition(){
@@ -271,16 +244,14 @@ function check_condition(){
    let condition_input = document.getElementById('checkbox1');
    let error_condition = document.getElementById('error_checkbox');
  
+   // si ma checkbox est sélectionnée 
    if(condition_input.checked){
+      // je n'affiche pas d'erreur et j'incrémente à true verifTab pour la vérification
      error_condition.style.display = "none";
      verifTab[6] = true;
-     console.log(true);
    }else{
+     // sinon j'affiche l'erreur et j'incrémente à false verifTab pour la vérification
      error_condition.style.display = "block";
-     console.log(false);
      verifTab[6] = false;
    }
-   console.log('La condition est elle true ou false ?' + verifTab[6]);
 }
-
-console.log(verifTab);
